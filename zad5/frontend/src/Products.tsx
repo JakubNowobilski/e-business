c2aad8d9a0c06b008015d48a5dd467483646436d
Jakub Nowobilski <nowobilski.jakub@gmail.com>
2024-06-08 17:43:51 +0200

    Add products fetched from REST API

---
+++ zad5/frontend/src/Products.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+interface Product {
+    id: number;
+    name: string;
+    price: number;
+    description: string;
+    image_url: string;
+}
+
+const Products: React.FC = () => {
+    const [products, setProducts] = useState<Product[]>([]);
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await axios.get<Product[]>('http://localhost:9000/products');
+                setProducts(response.data);
+            } catch (error) {
+                console.error('Failed to fetch products', error);
+            }
+        };
+
+        fetchProducts().then(() => {
+            console.log('Products fetched');
+        })
+    }, []);
+
+    return (
+        <div>
+            <p>Anything</p>
+            {products.map((product) => (
+                <div key={product.name}>
+                    <h2>{product.name}</h2>
+                    <img src={product.image_url} alt={product.name} />
+                    <p>{product.description}</p>
+                    <p>{product.price}</p>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Products;
