b6a977890926476206b9edb92573cff17102e1a6
Jakub Nowobilski <nowobilski.jakub@gmail.com>
2024-06-08 17:53:39 +0200

    Improve styling in zad5

--- zad5/frontend/src/Products.tsx
+++ zad5/frontend/src/Products.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import './Products.css';
 
 interface Product {
     id: number;
@@ -21,21 +22,19 @@ const Products: React.FC = () => {
                 console.error('Failed to fetch products', error);
             }
         };
-
         fetchProducts().then(() => {
             console.log('Products fetched');
         })
     }, []);
 
     return (
-        <div>
-            <p>Anything</p>
+        <div className="products-grid">
             {products.map((product) => (
-                <div key={product.name}>
+                <div key={product.name} className="product-card">
                     <h2>{product.name}</h2>
                     <img src={product.image_url} alt={product.name} />
                     <p>{product.description}</p>
-                    <p>{product.price}</p>
+                    <p>${product.price}</p>
                 </div>
             ))}
         </div>
